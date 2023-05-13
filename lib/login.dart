ed6868169b3f0e49197db3e0cf2e0cd6ab8970ff
ggradara <gabriele.gradara@studenti.unipd.it>
2023-05-13 15:22:24 +0200

    .

---
+++ lib/login.dart
@@ -0,0 +1,155 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class Diary extends StatelessWidget {
+  Diary({Key? key}) : super(key: key);
+
+  static const routename = 'HomePage';
+
+  @override
+  Widget build(BuildContext context) {
+    print('HomePage_temp built');
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('App name'),
+        //leading: Icon(Icons.menu),
+        //leadingWidth: 100,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "How do you feel today?",
+              style: TextStyle(  
+              fontSize: 18)),
+              SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                IconButton(
+                  iconSize: 60,
+                  icon: const Icon(Icons.sentiment_very_satisfied),
+                  onPressed: () {},
+                  color: Colors.green,
+                  ),
+                IconButton(
+                  iconSize: 60,
+                  icon: const Icon(Icons.sentiment_neutral),
+                  onPressed: () {},
+                  color: Colors.orange,
+                  ),                  
+                IconButton(
+                  iconSize: 60,
+                  icon: const Icon(Icons.sentiment_very_dissatisfied),
+                  onPressed: () {},
+                  color: Colors.red,
+                  ),
+              ]
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Container(
+              width: 250.0,
+              child: 
+                TextField(
+                  maxLines: 1,
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    color: Colors.black,
+                  ),
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    hintText: 'Username',
+                    hintStyle: TextStyle(
+                      fontSize: 16.0,
+                      color: Colors.white,
+                    ),
+                    contentPadding: EdgeInsets.symmetric(
+                      vertical: 16.0,
+                      horizontal: 16.0,
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                      borderSide: BorderSide.none,
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                      borderSide: BorderSide.none,
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                      borderSide: BorderSide(
+                        color: Colors.blue,
+                        width: 2.0,
+                      ),
+                    ),)),),
+            SizedBox(
+              height: 70,
+            ),
+            Container(
+              width: 250.0,
+              child: 
+                TextField(
+                  maxLines: 1,
+                  obscureText: true,
+                  obscuringCharacter: "*",
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    color: Colors.black,
+                  ),
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    hintText: 'Password',
+                    hintStyle: TextStyle(
+                      fontSize: 16.0,
+                      color: Colors.white,
+                    ),
+                    contentPadding: EdgeInsets.symmetric(
+                      vertical: 16.0,
+                      horizontal: 16.0,
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                      borderSide: BorderSide.none,
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                      borderSide: BorderSide.none,
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                      borderSide: BorderSide(
+                        color: Colors.blue,
+                        width: 2.0,
+                      ),
+                    ),)),),
+            SizedBox(
+              height: 50,
+            ),
+            ElevatedButton(
+                onPressed: (){},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                  fixedSize: const Size(200, 70),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30))),
+                child: Text(
+                  'Submit',
+                  style: TextStyle(  
+                    fontSize: 25)),  
+                    ),
+          ],
+        ),
+      ),
+    );
+  } //build
+} //HomePage
