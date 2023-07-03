335fc6a0c568499f69026eb0de81af91217e9be9
Tommaso Urbani <tommaso.urbani.promo@gmail.com>
2023-07-04 01:51:41 +0200

    Added sleep fetch, Sleep DB, update graph with day

--- lib/models/sleep.dart
+++ lib/models/sleep.dart
@@ -1,16 +1,35 @@
 class Sleep {
-  final DateTime startTime;
+  final String dateOfSleep;
+  final String startTime;
+  final String endTime;
+  final double duration;
   final int efficiency;
 
-  Sleep({required this.startTime, required this.efficiency}); //
+  Sleep(
+      {required this.dateOfSleep,
+      required this.startTime,
+      required this.endTime,
+      required this.duration,
+      required this.efficiency}); //
   factory Sleep.fromJson(Map<String, dynamic> json) {
-    return Sleep(startTime: json['startTime'], efficiency: json['efficiency']);
+    return Sleep(
+        dateOfSleep: json['dateOfSleep'],
+        startTime: json['startTime'],
+        endTime: json['endTime'],
+        duration: json['duration'],
+        efficiency: json['efficiency']);
   }
 
   int operator [](String key) {
     switch (key) {
+      case 'dateOfSleep':
+        return dateOfSleep as int;
       case 'startTime':
         return startTime as int;
+      case 'endTime':
+        return endTime as int;
+      case 'duration':
+        return duration as int;
       case 'efficiency':
         return efficiency;
       default:
@@ -20,6 +39,6 @@ class Sleep {
 
   @override
   String toString() {
-    return 'HeartRate(startTime: $startTime, efficiency: $efficiency)';
+    return 'Sleep(dateOfSleep: $dateOfSleep, startTime: $startTime, endTime: $endTime, duration: $duration, efficiency: $efficiency)'; //, error: $error
   } //toString
 }//HeartRate
